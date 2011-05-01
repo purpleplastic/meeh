0b8b051e0c8cec230fa85afca0b8658ece9a54e9
purpleplastic <jaali@nym.hush.com>
2011-05-01 17:27:05 +0100

    trying out

---
+++ requestHandlers.js
@@ -0,0 +1,21 @@
+var exec = require("child_process").exec;
+
+function start(response) {
+  console.log("Request handler 'start' was called.");
+
+  exec("ping -c 1 192.168.0.1", function (error, stdout, stderr) {
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(stdout.trim());
+    response.end();
+  });
+}
+
+function upload(response) {
+  console.log("Request handler 'upload' was called.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hello Upload");
+  response.end();
+}
+
+exports.start = start;
+exports.upload = upload;
