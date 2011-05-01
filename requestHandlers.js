ac2b679528fe210ad6829f8271db26c06b0628af
purpleplastic <jaali@nym.hush.com>
2011-05-01 17:41:50 +0100

    removed ping

--- requestHandlers.js
+++ requestHandlers.js
@@ -3,7 +3,7 @@ var exec = require("child_process").exec;
 function start(response) {
   console.log("Request handler 'start' was called.");
 
-  exec("ping -c 1 192.168.0.1", function (error, stdout, stderr) {
+  exec("ls", function (error, stdout, stderr) {
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write(stdout.trim());
     response.end();
